317723725a2017b16f9caa51bf5ff6fc2b19f5a4
Laura Jackson <playeronereadystart@gmail.com>
2019-01-02 17:41:36 +0000

    express with mongo

--- app.js
+++ app.js
@@ -1,125 +1,126 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import db from './db/db';
-
-const app = express();
-
-// Parse incoming requests data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//make new user
-app.post('/api/v1/users', (req, res) => {
-  if (!req.body.username) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'username is required'
-    });
-  } else if (!req.body.password) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'password is required'
-    });
-  } else if (db.filter(user => {
-    return user.username.toLowerCase() == req.body.username.toLowerCase()
-  }).length == 1) {
-      return res.status(400).send({
-        success: 'false',
-        message: 'username already exists!'
-    });
-  }
-  const userIDs = db.map(obj => { return obj.id });
-  console.log(userIDs);
-  const highestID = Math.max(...userIDs);
-    console.log(highestID);
-  const user = {
-    id: highestID + 1,
-    username: req.body.username,
-    password: req.body.password
-  }
-  db.push(user);
-  return res.status(200).send({
-    success: 'true',
-    message: 'user has been added',
-    user
-  })
-});
-
-//get all users
-app.get('/api/v1/users', (req, res) => {
-  res.status(201).send({
-    success: 'true',
-    message: 'users received',
-    users: db
-  })
-});
-
-// get individual
-app.get('/api/v1/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  db.map((user) => {
-    if (user.id === id) {
-      return res.status(200).send({
-        success: 'true',
-        message: 'user retrieved successfully',
-        user,
-      });
-    }
-  });
- return res.status(404).send({
-   success: 'false',
-   message: 'user does not exist',
- });
-});
-
-//delete users
-app.delete('/api/v1/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  db.map((user, index) => {
-    if (user.id === id) {
-       db.splice(index, 1);
-       return res.status(200).send({
-         success: 'true',
-         message: 'user deleted successfuly',
-       });
-    }
-  });
-    return res.status(404).send({
-      success: 'false',
-      message: 'user not found',
-    });
-});
-
-//update user password
-app.put('/api/v1/users', (req, res) => {
-  let username =  req.body.username.toLowerCase();
-  let password = req.body.password;
-  let newPassword = req.body.newPassword;
-  if ((!username) || (!password) || (!newPassword)) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'username, password and new password required'
-    })
-  }
-  db.map(user => {
-    if ((user.username.toLowerCase() == username) && (user.password == password)) {
-      user.password = newPassword;
-      return res.status(202).send({
-        success: 'true',
-        message: 'user has been updated'
-      })
-    }
-  })
-  return res.status(400).send({
-    success: 'false',
-    message: "user doesn't exist, or user or password didn't match"
-  })
-})
-
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+//MOVED TO MONGO
+// import bodyParser from 'body-parser';
+// import express from 'express';
+// import db from './db/db';
+//
+// const app = express();
+//
+// // Parse incoming requests data
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+// //make new user
+// app.post('/api/v1/users', (req, res) => {
+//   if (!req.body.username) {
+//     return res.status(400).send({
+//       success: 'false',
+//       message: 'username is required'
+//     });
+//   } else if (!req.body.password) {
+//     return res.status(400).send({
+//       success: 'false',
+//       message: 'password is required'
+//     });
+//   } else if (db.filter(user => {
+//     return user.username.toLowerCase() == req.body.username.toLowerCase()
+//   }).length == 1) {
+//       return res.status(400).send({
+//         success: 'false',
+//         message: 'username already exists!'
+//     });
+//   }
+//   const userIDs = db.map(obj => { return obj.id });
+//   console.log(userIDs);
+//   const highestID = Math.max(...userIDs);
+//     console.log(highestID);
+//   const user = {
+//     id: highestID + 1,
+//     username: req.body.username,
+//     password: req.body.password
+//   }
+//   db.push(user);
+//   return res.status(200).send({
+//     success: 'true',
+//     message: 'user has been added',
+//     user
+//   })
+// });
+//
+// //get all users
+// app.get('/api/v1/users', (req, res) => {
+//   res.status(201).send({
+//     success: 'true',
+//     message: 'users received',
+//     users: db
+//   })
+// });
+//
+// // get individual
+// app.get('/api/v1/users/:id', (req, res) => {
+//   const id = parseInt(req.params.id, 10);
+//   db.map((user) => {
+//     if (user.id === id) {
+//       return res.status(200).send({
+//         success: 'true',
+//         message: 'user retrieved successfully',
+//         user,
+//       });
+//     }
+//   });
+//  return res.status(404).send({
+//    success: 'false',
+//    message: 'user does not exist',
+//  });
+// });
+//
+// //delete users
+// app.delete('/api/v1/users/:id', (req, res) => {
+//   const id = parseInt(req.params.id, 10);
+//
+//   db.map((user, index) => {
+//     if (user.id === id) {
+//        db.splice(index, 1);
+//        return res.status(200).send({
+//          success: 'true',
+//          message: 'user deleted successfuly',
+//        });
+//     }
+//   });
+//     return res.status(404).send({
+//       success: 'false',
+//       message: 'user not found',
+//     });
+// });
+//
+// //update user password
+// app.put('/api/v1/users', (req, res) => {
+//   let username =  req.body.username.toLowerCase();
+//   let password = req.body.password;
+//   let newPassword = req.body.newPassword;
+//   if ((!username) || (!password) || (!newPassword)) {
+//     return res.status(400).send({
+//       success: 'false',
+//       message: 'username, password and new password required'
+//     })
+//   }
+//   db.map(user => {
+//     if ((user.username.toLowerCase() == username) && (user.password == password)) {
+//       user.password = newPassword;
+//       return res.status(202).send({
+//         success: 'true',
+//         message: 'user has been updated'
+//       })
+//     }
+//   })
+//   return res.status(400).send({
+//     success: 'false',
+//     message: "user doesn't exist, or user or password didn't match"
+//   })
+// })
+//
+//
+// const PORT = process.env.PORT || 8080;
+//
+// app.listen(PORT, () => {
+//   console.log(`server running on port ${PORT}`)
+// })
---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var MongoClient = require('mongodb').MongoClient
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var db;
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+
+//generic error handler used by all endpoints
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("/users", function(req, res) {
+  MongoClient.connect(process.env.MONGOLAB_URI,{ useNewUrlParser: true }, function (err, client) {
+    if (err) throw err
+    db = client.db('fof-users')
+
+    db.collection('users').find().toArray(function (err, result) {
+        return res.status(201).send({
+          success: 'true',
+          message: 'users received',
+          users: result.map(user => user.username)
+        })
+    });
+  });
+});
+//
+// app.post("/testing", function(req, res) {
+//   MongoClient.connect(MONGODB_URI,{ useNewUrlParser: true }, function (err, client) {
+//     if (err) throw err
+//     db = client.db('fof-users')
+//
+//     db.collection('users').find().toArray(function (err, result) {
+//       if (err) throw err
+//       return result
+//     });
+//   });
+// });
+
+app.post("/createuser", function(req, res) {
+  MongoClient.connect(process.env.MONGOLAB_URI,{ useNewUrlParser: true }, function (err, client) {
+    db = client.db('fof-users');
+
+    db.collection('users').find().toArray(function (err, result) {
+      //check for blank username or password
+      if ((!req.body.username || !req.body.password )) {
+         return handleError(res, "Invalid entry", "Must provide a username and a password!", 400);
+      }
+      //checks if username is in use already in database
+      if (result.filter(user => { return user.username == req.body.username.toLowerCase();}).length >= 1) {
+        return handleError(res, 'Username must be unique', 'username already exists!', 400);
+      }
+      // adds new entry if passes through previous checks.
+      db.collection('users').insertOne({
+        username: req.body.username.toLowerCase(),
+        password: req.body.password
+      }, function(err, doc) {
+        if (err) {
+          return handleError(res, err.message, "Failed to create new user");
+        } else {
+          return res.status(201).json(doc.ops[0])
+        }
+      });
+    });
+  });
+})
