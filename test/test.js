862800731ddb5ec9282ec9c560586f9499306ec4
Laura Jackson <playeronereadystart@gmail.com>
2019-01-06 18:43:17 +0000

    Delete test.js

--- test/test.js
+++
@@ -1,8 +0,0 @@
-//
-// const chai = require("chai");
-// const expect = chai.expect;
-// describe('dummy test for travis', function() {
-//     it('dummy test to return true', function() {
-//        expect(true).to.be.true;
-//     });
-// });
